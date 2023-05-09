a580b95c92e2695811666897331a4e1e54697ae8
Ayush Gaba <ayuxh@amazon.com>
2023-05-10 05:03:17 +0530

    add zap

---
+++ zsh/plugins.zsh
@@ -0,0 +1,5 @@
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+plug "zsh-users/zsh-autosuggestions"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "marlonrichert/zsh-autocomplete"
+plug "jeffreytse/zsh-vi-mode"
