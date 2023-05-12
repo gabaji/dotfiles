00fef0c89c4ebc28179778a766afd07a7653edf9
ayushgaba71@gmail.com <ayushgaba71@gmail.com>
2023-05-13 03:26:03 +0530

    fix order issues with plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,8 +1,7 @@
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+plug "jeffreytse/zsh-vi-mode"
+plug "zap-zsh/exa"
 plug "zsh-users/zsh-autosuggestions"
 plug "zsh-users/zsh-syntax-highlighting"
 plug "marlonrichert/zsh-autocomplete"
-plug "jeffreytse/zsh-vi-mode"
-plug "romkatv/powerlevel10k"
-plug "zap-zsh/exa"
 
